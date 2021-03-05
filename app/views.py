97f4028cdf6d30c21d4c39dc0b32f2c84686b82f
Kalia-Lee Rodney <kaliarodney@gmail.com>
2021-03-04 21:49:18 -0500

    Logout added

--- app/views.py
+++ app/views.py
@@ -38,6 +38,7 @@ def login():
     if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
+        
         if form.username.data and form.password.data:
             username = form.username.data
             password = form.password.data
@@ -45,24 +46,41 @@ def login():
 
             # Get the username and password values from the form.
             if user is not None and check_password_hash(user.password, password):
+                user = UserProfile.query.filter_by(username=username).first()
+                id = load_user(1)
+
+               
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
-
+                
             # get user id, load into session
                 login_user(user)
                 flash("Login Successful","Success")
+            
 
             # remember to flash a message to the user
                 return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash("Something went wrong.Try again")
+    
+
     return render_template("login.html", form=form)
 
 @app.route("/secure-page")
 @login_required
 def secure_page():
     return render_template("secure_page.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+        flash("You are now logged out","You cannot enter")
+        return redirect(url_for('home'))
 
 
 
